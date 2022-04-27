cdfae61f92fa170792ec2e0f6b2a56af50c0b2cd
notmoaiad <notmoaiad@gmail.com>
2022-04-27 16:21:52 +0300

    added clear for the termenal

--- yt.py
+++ yt.py
@@ -1,9 +1,15 @@
 import subprocess
-from os import system, path
+from os import system, path, name
 import signal
 import sys
 args = sys.argv[1:]
 args = ' '.join(args)
+
+def clear_termenal():
+	if name.startswith("nt"):
+		system("cls")
+	else:
+		system("clear")
 
 def handler(signum, frame):
         system(f"pkill -f {path.basename(__file__)}")
@@ -13,12 +19,12 @@ history = []
 
 while True:
     if not args:
-    	search_query_org = input("Search Query: ")
+        search_query_org = input("Search Query: ")
     else:
-    	search_query_org = args
-    	args = None
+        search_query_org = args
+        args = None
     search_query = search_query_org.strip("ao")
-
+    clear_termenal()
     if search_query == "history":
         out = subprocess.check_output(['ytfzf', '-I', 'R', '-q'])
     elif not search_query:
@@ -26,7 +32,7 @@ while True:
             continue
         out = subprocess.check_output(['ytfzf', '-I', 'R', history[len(history)-1]])
     else: 
-        out = subprocess.check_output(['ytfzf', '-I', 'R', search_query])
+        out = subprocess.check_output(['ytfzf','-I', 'R', search_query])
         history.append(search_query)
 
     out = out.decode('utf-8')
@@ -35,7 +41,7 @@ while True:
     print("\nPlaying:", title, "\n", "link:", link)
 
     if search_query_org.startswith("ao"):
-    	system("mpv --no-video --loop -fs --screen=1 --ytdl-raw-options='sub-lang=\"en\",write-sub=,write-auto-sub=' "+ link)
+        system("mpv --no-video --loop -fs --screen=1 --ytdl-raw-options='sub-lang=\"en\",write-sub=,write-auto-sub=' "+ link)
     else:
 
-    	system("mpv --loop -fs --screen=1 --ytdl-raw-options='sub-lang=\"en\",write-sub=,write-auto-sub=' "+ link)
+        system("mpv --loop -fs --screen=1 --ytdl-raw-options='sub-lang=\"en\",write-sub=,write-auto-sub=' "+ link)
